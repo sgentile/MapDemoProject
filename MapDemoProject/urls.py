aa7e98a059f1ce39d710b658fb73a7f00da51575
Steve Gentile <steven.gentile@gmail.com>
2013-09-26 16:01:06 -0400

    added default index page view

--- MapDemoProject/urls.py
+++ MapDemoProject/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import TemplateView
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'MapDemoProject.views.home', name='home'),
     # url(r'^MapDemoProject/', include('MapDemoProject.foo.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
